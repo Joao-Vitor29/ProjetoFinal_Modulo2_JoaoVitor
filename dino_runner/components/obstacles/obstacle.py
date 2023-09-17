b52dd20cb267c9a95ee867b472af596cae06f394
Joao-Vitor29 <jvmendonca29@hotmail.com>
2023-09-17 12:45:29 -0300

    Att Dino Funcionando, implentando mudanças

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -1,8 +1,6 @@
 import pygame
-
 from pygame.sprite import Sprite
 from dino_runner.utils.constants import SCREEN_WIDTH
-
 
 class Obstacle(Sprite):
     def __init__(self, image, type):
@@ -10,13 +8,12 @@ class Obstacle(Sprite):
         self.type = type
         self.rect = self.image[self.type].get_rect()
         self.rect.x = SCREEN_WIDTH
-
+        
     def update(self, game_speed, obstacles):
         self.rect.x -= game_speed
-
-        if self.rect.x < -self.rect.width:
+        
+        if self.rect.x < - self.rect.width:
             obstacles.pop()
-
+            
     def draw(self, screen):
-        screen.blit(self.image[self.type], (self.rect.x, self.rect.y))
         screen.blit(self.image[self.type], (self.rect.x, self.rect.y))
